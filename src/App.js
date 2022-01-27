a918975564656709a054a2ea90242d7053607904
jember08 <afandayul@gmail.com>
2022-01-28 05:02:43 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useCallback, useEffect, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import axios from "axios";
 
 function App() {
+  const [images, setImages] = useState([]);
+
+  function handleUpload() {
+    console.log("uploading file....");
+    axios
+      .post("http://localhost:4000/upload", { images })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }
+
+  const onDrop = useCallback((acceptedFiles, rejectedFiles) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImages((prevState) => [...prevState, reader.result]);
+      };
+      reader.readAsDataURL(file);
+    });
+    console.log("acceptedFiles", acceptedFiles);
+    console.log("rejectedFiles", rejectedFiles);
+  }, []);
+
+  useEffect(() => {
+    console.log(images);
+  }, [images]);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: "image/png",
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="dropzone" {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? "Drag Active" : "Drag and drop a file here"}
+      </div>
+      {images.length > 0 && (
+        <div>
+          {images.map((image, index) => (
+            <img className="selected-images" src={image} key={index} alt="" />
+          ))}
+        </div>
+      )}
+      {images.length > 0 && (
+        <button onClick={handleUpload}>upload images</button>
+      )}
     </div>
   );
 }
